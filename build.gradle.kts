bc07ba9a48ec83830dca858906828bab56f3bf6c
Alexander Krauss <alexander.krauss@qaware.de>
2020-03-15 21:03:40 +0100

    Upgraded Gradle; added README.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    testCompile("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.assertj:assertj-core:3.11.1")
 }
